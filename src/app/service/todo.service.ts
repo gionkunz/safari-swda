81ca427d9349246d93930c841cf99de4b41da766
Gion Kunz <gion.kunz@oddeven.ch>
2017-10-17 12:04:49 +0200

    Reuced to lab 9 excercise

--- src/app/service/todo.service.ts
+++ src/app/service/todo.service.ts
@@ -8,27 +8,33 @@ export class TodoService {
   url = 'http://localhost:8080';
 
   loadTodos(): Observable<TodoItem[]> {
-    return this.http
-      .get(`${this.url}/todos`)
-      .map((response) => <TodoItem[]>response.json());
+    // TODO: Create the necessary call to the backend
+    // Examples: https://angular.io/tutorial/toh-pt6
+    // URL: GET http://localhost:8080/todos
+    return Observable.of([]);
   }
 
   loadTodo(nr: string): Observable<TodoItem> {
-    return this.http
-      .get(`${this.url}/todos/${nr}`)
-      .map((response) => <TodoItem>response.json());
+    // TODO: Create the necessary call to the backend
+    // Examples: https://angular.io/tutorial/toh-pt6
+    // URL: GET http://localhost:8080/todos/:nr
+    return Observable.of({});
   }
 
   createTodo(data: any): Observable<TodoItem> {
-    return this.http
-      .put(`${this.url}/todos`, data)
-      .map((response) => <TodoItem>response.json());
+    // TODO: Create the necessary call to the backend
+    // Examples: https://angular.io/tutorial/toh-pt6
+    // URL: PUT http://localhost:8080/todos
+    // Data: {title: string; description: string; done: boolean}
+    return Observable.of({});
   }
 
   updateTodo(nr: string, data: any): Observable<TodoItem> {
-    return this.http
-      .post(`${this.url}/todos/${nr}`, data)
-      .map((response) => <TodoItem>response.json());
+    // TODO: Create the necessary call to the backend
+    // Examples: https://angular.io/tutorial/toh-pt6
+    // URL: POST http://localhost:8080/todos/:nr
+    // Data: {title: string; description: string; done: boolean}
+    return Observable.of({});
   }
 
   constructor(@Inject(Http) private http: Http) {}
