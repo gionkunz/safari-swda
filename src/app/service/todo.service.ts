f7763d73dc8684cec1b619dfc6e65816fdd21b28
Gion Kunz <gion.kunz@oddeven.ch>
2017-10-17 12:04:49 +0200

    Reuced to lab 9 excercise

--- src/app/service/todo.service.ts
+++ src/app/service/todo.service.ts
@@ -3,32 +3,44 @@ import {Http} from '@angular/http';
 import {TodoItem} from '../model/todo';
 import {Observable} from 'rxjs/Rx';
 
+/**
+ * Inject Http into your TodoService
+ * ---------------------------------
+ * You can use `@Inject(Http) private http: Http` in your constructor to
+ * obtain the Http service.
+ *
+ * Create the necessary calls within TodoService to the backend
+ * ------------------------------------------------------------
+ * Implement the calls to the backend correctly using the following URLS:
+ * GET http://localhost:8080/todos
+ * PUT http://localhost:8080/todos
+ *     BODY: {title: string; description: string; done: boolean}
+ * GET http://localhost:8080/todos/:id
+ * POST http://localhost:8080/todos/:id
+ *     BODY: {title: string; description: string; done: boolean}
+ *
+ * Examples: https://angular.io/tutorial/toh-pt6
+ *
+ */
+
 @Injectable()
 export class TodoService {
   url = 'http://localhost:8080';
 
   loadTodos(): Observable<TodoItem[]> {
-    return this.http
-      .get(`${this.url}/todos`)
-      .map((response) => <TodoItem[]>response.json());
+    return Observable.of([]);
   }
 
   loadTodo(nr: string): Observable<TodoItem> {
-    return this.http
-      .get(`${this.url}/todos/${nr}`)
-      .map((response) => <TodoItem>response.json());
+    return Observable.of({});
   }
 
   createTodo(data: any): Observable<TodoItem> {
-    return this.http
-      .put(`${this.url}/todos`, data)
-      .map((response) => <TodoItem>response.json());
+    return Observable.of({});
   }
 
   updateTodo(nr: string, data: any): Observable<TodoItem> {
-    return this.http
-      .post(`${this.url}/todos/${nr}`, data)
-      .map((response) => <TodoItem>response.json());
+    return Observable.of({});
   }
 
   constructor(@Inject(Http) private http: Http) {}
